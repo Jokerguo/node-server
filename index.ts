305606ac5291079c8bbd7837407af9c49290dc09
郭东生 <1977036313@qq.com>
2022-02-25 15:21:24 +0800

    返回文件

--- index.ts
+++ index.ts
@@ -1,29 +1,37 @@
 import * as http from 'http'
 import { IncomingMessage, ServerResponse } from 'http'
 import * as fs from 'fs'
+import * as p from 'path'
 
 const server = http.createServer()
+// public文件绝对路径
+const publicDir = p.resolve(__dirname, 'public') //__dirname 当前文件所在目录
 
 server.on('request', (request: IncomingMessage, response: ServerResponse) => {
-  console.log('method', request.method)
-  console.log('url', request.url)
-  console.log('headers', request.headers)
-  const arr = []
-  request.on('data', (chunk) => {
-    arr.push(chunk)
-  })
-  request.on('end', () => {
-    // body: post请求的参数
-    const body = Buffer.concat(arr).toString()
-    response.statusCode = 200
-    response.setHeader('X-Guo', `I'm guo`)
-    response.setHeader('Content-Type', 'image/png')
-    const img = fs.readFileSync('./11.png', 'binary')
-    response.write(img, 'binary')
-    response.end()
-  })
+  const { method, url, headers } = request
+  switch (url) {
+    case '/index.html':
+      response.setHeader('Content-Type', 'text/html; charset=utf-8')
+      fs.readFile(p.resolve(publicDir, 'index.html'), (error, data) => {
+        if (error) throw error
+        response.end(data.toString()) //data： Buffer
+      })
+      break
+    case '/style.css':
+      response.setHeader('Content-Type', 'text/css; charset=utf-8')
+      fs.readFile(p.resolve(publicDir, 'style.css'), (error, data) => {
+        if (error) throw error
+        response.end(data.toString()) //data： Buffer
+      })
+      break
+    case '/main.js':
+      response.setHeader('Content-Type', 'text/javascript; charset=utf-8')
+      fs.readFile(p.resolve(publicDir, 'main.js'), (error, data) => {
+        if (error) throw error
+        response.end(data.toString()) //data： Buffer
+      })
+      break
+  }
 })
 
-server.listen(8888, () => {
-  console.log(server.address())
-})
+server.listen(8888)
